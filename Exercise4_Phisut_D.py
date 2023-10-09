30a2c0de2f05f9e26f5b89baf37921a12aa0310e
sadnsidja <113336853+sadnsidja@users.noreply.github.com>
2023-10-09 14:19:25 +0700

    Create Exercise4_Phisut_D.py

---
+++ Exercise4_Phisut_D.py
@@ -0,0 +1,10 @@
+FE   = 50.7 #Foundation English
+GB   = 45.2 #General Business
+ITCS = 50.2 #Introduction to Computer Systems
+CP   = 90   #Computer Programming
+
+print((" "*10) + "--- Your Score ---")
+print("Foundation English               :",FE)
+print("General Business                 :",GB)
+print("Introduction to Computer Systems :",ITCS)
+print("Computer Programming             :",CP)
