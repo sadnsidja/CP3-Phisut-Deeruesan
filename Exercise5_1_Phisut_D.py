5df948a63ce79c050ed7703bc10d263c3c50df17
sadnsidja <113336853+sadnsidja@users.noreply.github.com>
2023-12-02 14:08:20 +0700

    Create Exercise5_1_Phisut_D.py

---
+++ Exercise5_1_Phisut_D.py
@@ -0,0 +1,6 @@
+Num1 = int(input())
+Num2 = int(input())
+print(Num1,"+",Num2,"=",Num1 + Num2)
+print(Num1,"-",Num2,"=",Num1 - Num2)
+print(Num1,"*",Num2,"=",Num1 * Num2)
+print(Num1,"/",Num2,"=",int(Num1 / Num2))
